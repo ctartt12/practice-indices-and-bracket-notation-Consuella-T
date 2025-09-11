057b6298e66802a40d6b8d980bd7aa210c03dc48
ctartt12 <consuellat12@gmail.com>
2025-09-11 04:51:41 +0000

    complete

---
+++ array-notations.js
@@ -0,0 +1,24 @@
+const libraryShelves = [
+    ['twilight', 'insideout', 'falling star'],
+    ['little engine', 'chicka chicka boom boom', 'coraline'], 
+    ['the matrix', 'the wishing star', 'jurasic park'],
+]
+console.log(libraryShelves[0][0])
+console.log(libraryShelves[0][1])
+console.log(libraryShelves[0][2])
+console.log(libraryShelves[1][0])
+console.log(libraryShelves[1][1])
+console.log(libraryShelves[1][2])
+console.log(libraryShelves[2][0])
+console.log(libraryShelves[2][1])
+console.log(libraryShelves[2][2])
+
+for (let row = 0; row < libraryShelves.length; row++) {
+  for (let item = 0; item < libraryShelves[row].length; item++) {
+    console.log(libraryShelves[row][item])
+  }
+}
+
+for (let i = 0; i < libraryShelves[1].length; i++) {
+  console.log(libraryShelves[1][i])
+}
