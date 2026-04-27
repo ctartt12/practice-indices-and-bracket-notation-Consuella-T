25a6675861a6e4dacc15e1a5512fcce9a8e00887
ctartt12 <consuellat12@gmail.com>
2026-04-27 16:38:54 -0500

    Refactor libraryShelves and enhance console logs
    Updated book titles and improved code formatting for better readability.

--- array-notations.js
+++ array-notations.js
@@ -1,24 +1,30 @@
 const libraryShelves = [
-    ['twilight', 'insideout', 'falling star'],
-    ['little engine', 'chicka chicka boom boom', 'coraline'], 
-    ['the matrix', 'the wishing star', 'jurasic park'],
-]
-console.log(libraryShelves[0][0])
-console.log(libraryShelves[0][1])
-console.log(libraryShelves[0][2])
-console.log(libraryShelves[1][0])
-console.log(libraryShelves[1][1])
-console.log(libraryShelves[1][2])
-console.log(libraryShelves[2][0])
-console.log(libraryShelves[2][1])
-console.log(libraryShelves[2][2])
+  ['twilight', 'insideout', 'falling star'],
+  ['little engine', 'chicka chicka boom boom', 'Star Wars'], 
+  ['Crazy Rich Asians', 'the wishing star', 'jurassic park'],
+];
 
+// Task: Access specific books (cleaned up)
+console.log(libraryShelves[0][0]);
+console.log(libraryShelves[0][1]);
+console.log(libraryShelves[0][2]);
+
+console.log(libraryShelves[1][0]);
+console.log(libraryShelves[1][1]);
+console.log(libraryShelves[1][2]);
+
+console.log(libraryShelves[2][0]);
+console.log(libraryShelves[2][1]);
+console.log(libraryShelves[2][2]);
+
+// Task: Loop through entire library (best practice)
 for (let row = 0; row < libraryShelves.length; row++) {
   for (let item = 0; item < libraryShelves[row].length; item++) {
-    console.log(libraryShelves[row][item])
+    console.log(libraryShelves[row][item]);
   }
 }
 
+// Task: Loop only second shelf
 for (let i = 0; i < libraryShelves[1].length; i++) {
-  console.log(libraryShelves[1][i])
+  console.log(libraryShelves[1][i]);
 }
